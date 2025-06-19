b2ebb7e9d0ffc9ab1383f7a9c376b9c652712f37
Akshata <akshata@hubsystems.com.au>
2025-06-20 09:37:04 +1000

    updated category table signature to match backend updated migration script to include table name

--- app/src/main/java/com/example/transcribeassistant/data/cache/entity/Category.kt
+++ app/src/main/java/com/example/transcribeassistant/data/cache/entity/Category.kt
@@ -3,7 +3,7 @@ package com.example.transcribeassistant.data.cache.entity
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "categories")
+@Entity(tableName = "category")
 data class Category(
     @PrimaryKey val id: String,
     val name: String
