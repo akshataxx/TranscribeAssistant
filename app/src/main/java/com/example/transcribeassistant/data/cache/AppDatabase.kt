2abeae75a7309abd139e4eabe3c45e60489787b1
Arjun <arjun.pushpavannan@gmail.com>
2025-06-21 00:59:36 +1000

    clean

--- app/src/main/java/com/example/transcribeassistant/data/cache/AppDatabase.kt
+++ app/src/main/java/com/example/transcribeassistant/data/cache/AppDatabase.kt
@@ -3,15 +3,13 @@ package com.example.transcribeassistant.data.cache
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.example.transcribeassistant.data.cache.dao.TranscriptDao
-import com.example.transcribeassistant.data.cache.entity.Category
 import com.example.transcribeassistant.data.cache.entity.TranscriptEntity
 
 @Database(
-    entities = [TranscriptEntity::class, Category::class],
+    entities = [TranscriptEntity::class],
     version = 2,
     exportSchema = false
 )
 abstract class AppDatabase : RoomDatabase() {
     abstract fun transcriptDao(): TranscriptDao
-
 }
--- app/src/main/java/com/example/transcribeassistant/ui/viewmodel/TranscriptViewModel.kt
+++ app/src/main/java/com/example/transcribeassistant/ui/viewmodel/TranscriptViewModel.kt
@@ -40,5 +40,4 @@ class TranscriptViewModel @Inject constructor(
             }
         }
     }
-
 }
