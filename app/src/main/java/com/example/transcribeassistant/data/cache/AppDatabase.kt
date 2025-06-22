1376067ed6cdad9e8a34df81ef934793588a840e
Arjun <arjun.pushpavannan@gmail.com>
2025-06-22 15:22:23 +1000

    fix

--- app/src/main/java/com/example/transcribeassistant/data/cache/AppDatabase.kt
+++ app/src/main/java/com/example/transcribeassistant/data/cache/AppDatabase.kt
@@ -3,15 +3,19 @@ package com.example.transcribeassistant.data.cache
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import com.example.transcribeassistant.data.cache.dao.CategoryDao
 import com.example.transcribeassistant.data.cache.dao.TranscriptDao
+import com.example.transcribeassistant.data.cache.entity.Category
 import com.example.transcribeassistant.data.cache.entity.TranscriptEntity
 
 @Database(
-    entities = [TranscriptEntity::class],
+    entities = [TranscriptEntity::class, Category::class],
     version = 3,
     exportSchema = false
 )
 @TypeConverters(Converters::class)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun transcriptDao(): TranscriptDao
+
+    abstract fun categoryDao(): CategoryDao
 }
