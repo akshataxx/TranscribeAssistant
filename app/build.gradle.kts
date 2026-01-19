a49b8020d83e4b7ea89bb1aa1f0ba74a5a3fa202
Akshata <akshata@hubsystems.com.au>
2026-01-19 15:27:22 +1100

    app update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "com.contentcategorise.transcribeassistant"
         minSdk = 26
         targetSdk = 35
-        versionCode = 3
-        versionName = "1.0.2"
+        versionCode = 4
+        versionName = "1.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
